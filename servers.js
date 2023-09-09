e4da37a29fa5a4c06011713f77b867a5c83fcc6a
Xiuqi Feng <106839860+Xiuqifeng@users.noreply.github.com>
2023-09-08 18:00:50 -0700

    Update servers.js

--- servers.js
+++ servers.js
@@ -44,11 +44,11 @@ app.use('/api/v1/transections', require("./routes/transectionRoutes"))
 /*
     Static files
 */
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('*', function (req,res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-}) 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 /* 
     port
